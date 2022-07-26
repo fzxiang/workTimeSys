baa1aa3692c4135bb5e1fcd6735865d18fd22aca
傅子湘 <fuzx@xmpaoyou.com>
2022-07-26 10:26:49 +0800

    build: 打包

--- dist/assets/NotFound-legacy.e6a63993.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.4bb0a27f.js"],(function(e){"use strict";var n,t,r;return{setters:[function(e){n=e.a4,t=e.a7,r=e.ac}],execute:function(){e("default",n({},[["render",function(e,n){return t(),r("div",null,"not found")}]]))}}}));
