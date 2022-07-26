f913feb5c12563fec83c8d848f6befdf5e107c43
傅子湘 <fuzx@xmpaoyou.com>
2022-07-26 14:12:23 +0800

    build: 打包

---
+++ dist/assets/NotFound-legacy.fd21996e.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.8c0660db.js"],(function(e){"use strict";var n,t,r;return{setters:[function(e){n=e.a4,t=e.a7,r=e.ac}],execute:function(){e("default",n({},[["render",function(e,n){return t(),r("div",null,"not found")}]]))}}}));
--- dist/assets/NotFound.555f9395.js
+++
@@ -1,1 +0,0 @@
-import{a4 as e,a7 as n,ac as o}from"./index.d34e1cbc.js";const a={};function c(r,t){return n(),o("div",null,"not found")}var _=e(a,[["render",c]]);export{_ as default};
