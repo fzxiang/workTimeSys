66fdd5fbb20d89cc15a89e9c93647cea948848a5
傅子湘 <fuzx@xmpaoyou.com>
2022-08-24 11:33:26 +0800

    feat: 项目说明页功能

--- src/views/helper/index.vue
+++ src/views/helper/index.vue
@@ -28,24 +28,53 @@
 
   <div class="card">
     <h1>2、如何填写工时？</h1>
-    <p class="content">1、正在建设中...</p>
-    <p class="content">2、正在建设中...</p>
-    <p class="content">3、正在建设中...</p>
-    <p class="content">4、正在建设中...</p>
+    <p class="content">1、选择当前日期（下拉工时面板可展开日期）</p>
+    <p class="content">2、点击下方新增项目</p>
+    <p class="content">3、编辑工时，当天总工时必须等于100%或者0%</p>
+    <p class="content">
+      4、点击提交<span class="warning">（节假日情况下有提示弹窗 - 点击确认）</span>
+    </p>
+    <p class="content notice">注：操作提交后，会记录上一次提交摸版</p>
   </div>
 
   <div class="card">
     <h1>3、如何修改工时？</h1>
-    <p class="content">正在建设中...</p>
+    <p class="content">点击下方修改，进入编辑状态 - 修改完成点击提交</p>
+    <div class="van-video-box">
+      <video-player
+        class="video-player vjs-big-play-centered"
+        src="3b7aefca.mov"
+        crossorigin="anonymous"
+        playsinline
+        controls
+        :volume="0.6"
+        :playback-rates="[0.7, 1.0, 1.5, 2.0]"
+      />
+    </div>
   </div>
 
   <div class="card">
     <h1>4、如何重置（撤销）工时？</h1>
-    <p class="content">正在建设中...</p>
+    <p class="content">选择已填日期 - 点击下方重置按钮 - 确定</p>
+    <div class="van-video-box">
+      <video-player
+        class="video-player vjs-big-play-centered"
+        src="413bc5.mov"
+        crossorigin="anonymous"
+        playsinline
+        controls
+        :volume="0.6"
+        :playback-rates="[0.7, 1.0, 1.5, 2.0]"
+      />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VideoJsPlayer } from 'video.js'
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+</script>
 
 <style lang="less" scoped>
 .header {
@@ -71,6 +100,21 @@
     .van-tag {
       text-indent: 0;
     }
+  }
+  .van-video-box {
+    background-color: var(--van-background);
+    border-radius: var(--van-radio-size);
+    overflow: hidden;
+    .video-player {
+      width: 100%;
+      height: 200px;
+    }
+  }
+  .warning {
+    color: var(--van-warning-color);
+  }
+  .notice {
+    color: var(--van-gray-5)
   }
 }
 </style>
